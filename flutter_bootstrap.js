450eab15ea3967339e9859428dd9ceb5619e8a13
Behnam <behnami@revotec.eu>
2026-05-07 23:53:52 +0300

    deploy update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2981712494"
+    serviceWorkerVersion: "1474198888"
   }
 });
